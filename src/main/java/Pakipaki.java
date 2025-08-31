69cfec77b7b5a12be84622d5d7b8c9050311d1b4
freshwaterlemon <nguiyaksiong@gmail.com>
2025-08-31 23:42:42 +0800

    implement an initial skeletal version

---
+++ src/main/java/Pakipaki.java
@@ -0,0 +1,14 @@
+public class Pakipaki {
+    public static void main(String[] args) {
+        final String horizontalLine = "____________________________________________________________";
+        final String botName = "PakiPaki";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello I am " + botName + ", your personal assistant ChatBot.");
+        System.out.println("How can I help you today?");
+        System.out.println(horizontalLine);
+        System.out
+                .println("Thank you for chatting with me.\n" + "Goodbye, and I look forward to our next conversation!");
+        System.out.println(horizontalLine);
+    }
+}
