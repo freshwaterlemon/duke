985aa0807004a73a179746464fe1c83f963aeb46
freshwaterlemon <nguiyaksiong@gmail.com>
2025-09-01 23:50:39 +0800

    level 1 task, Echo

--- src/main/java/Pakipaki.java
+++ src/main/java/Pakipaki.java
@@ -1,25 +1,49 @@
+import java.util.Scanner;
+
 public class Pakipaki {
     private static final String BOTNAME = "PakiPaki";
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
-    public static void startMsg() {
-        System.out.println("Hello I am " + BOTNAME + ", your personal assistant ChatBot.");
-        System.out.println("How can I help you today?");
-    }
-
-    public static void endMsg() {
-        System.out.println("Thank you for chatting with me.\nGoodbye, and I look forward to our next conversation!");
-    }
-
+    // Print the horizontal decoration line
     public static void printHorzLine() {
         System.out.println(HORIZONTAL_LINE);
     }
 
+    // Print start message
+    public static void startMsg() {
+        printHorzLine();
+        System.out.println("Hello I am " + BOTNAME + ", your personal assistant ChatBot.");
+        System.out.println("How can I help you today? (type 'bye' to exit)");
+        printHorzLine();
+    }
+
+    // Print end message
+    public static void endMsg() {
+        printHorzLine();
+        System.out.println("Thank you for chatting with me.\nGoodbye, and I look forward to our next conversation!");
+        printHorzLine();
+    }
+
+    // Get user input and display them
+    public static void handleUserInput() {
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a task: ");
+            userInput = in.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                endMsg();
+                break;
+            }
+
+            System.out.println("Task to remember: " + userInput + "\n");
+
+        }
+    }
+
     public static void main(String[] args) {
-        printHorzLine();
         startMsg();
-        printHorzLine();
-        endMsg();
-        printHorzLine();
+        handleUserInput();
     }
 }
